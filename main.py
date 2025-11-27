e747a11b00eb0d54723cbec7bea7106ce3690865
Nayhely Valle <ngt.valle@yavirac.edu.ec>
2025-11-27 10:05:44 -0500

    Prueba 5

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "¡Hola conoce nuestra prueba de Kubernetes con CI/CD automatizado!"}
+    return {"message": "¡Hola bienvenido a nuestra prueba de Kubernetes con CI/CD automatizado!"}
 
 @app.get("/status")
 def status():
